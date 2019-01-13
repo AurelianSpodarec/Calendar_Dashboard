f8750206637ac2cf7f31f05bbfc70390739af991
Aurelian <aurelians@libero.it>
2019-01-13 20:29:36 +0000

    added icons, fixed the footer collapse

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
-import '../styles/index.scss';
+import fontawesome from '@fortawesome/fontawesome'
+import regular from '@fortawesome/fontawesome-free-regular'
+import solid from '@fortawesome/fontawesome-free-solid'
+import brands from '@fortawesome/fontawesome-free-brands'
 
-console.log('webpack starterkit');
+fontawesome.library.add(regular)
+fontawesome.library.add(solid)
+fontawesome.library.add(brands)
+
+import '../styles/index.scss';
