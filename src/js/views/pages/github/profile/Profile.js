73dfe42c3ba0057516204429de0166804e533cef
Aurelian <aurelians@libero.it>
2019-01-24 22:47:57 +0000

    changed the UI view for github profile

--- src/js/views/pages/github/profile/Profile.js
+++ src/js/views/pages/github/profile/Profile.js
@@ -50,81 +50,84 @@ let GithubProfile = {
             <h1> Github Profile </h1>
             
             <div class="github-user-profile">
-                
-                <div class="github-user-profile__img-wrap">
-                    <img class="github-user-profile__img" src="https://avatars2.githubusercontent.com/u/10155855?s=460&v=4" alt=""/>
-                </div>
 
                 <div class="github-user-profile__details">
-                    <div class="github-user-profile__info">
-                        <h1>Aurelian Spodarec</h1>
-                        <span>@AurelianSpodarec</span>
-                    </div>
-                <div class="github-user-profile__bio">
-                    <p>Apparently, this user prefers to keep an air of mystery about them. - You can always visit my website and find out more about how I can help you! 🥇 💯 👍 🥇 💯</p>
-                </div>
-                <ul class="github-details">
-                    <li class="github-details__item">
-                        <span class="github-details__icon"><i class="fas fa-home"></i></span>
-                        <span class="github-details__name">LoveToCode</span>
-                    </li>
-                    <li class="github-details__item">
-                        <span class="github-details__icon"><i class="fas fa-map-marker-alt"></i></span>
-                        <span class="github-details__name">Poland/UK</span>
-                    </li>
-                    <li class="github-details__item">
-                        <span class="github-details__icon"><i class="far fa-envelope"></i></span>
-                        <a class="github-details__link" href="mailto:aurelianxspodarec@gmail.com">aurelianxspodarec@gmail.com</a>
-                    </li>
-                    <li class="github-details__item">
-                        <span class="github-details__icon"><i class="fas fa-link"></i></span>
-                        <a class="github-details__link" href="http://aurelianspodarec.co.uk/">http://aurelianspodarec.co.uk/</a>
-                    </li>
-                </ul>
 
-                <div class="speciality">
-                    <span class="speciality__title">Topics & Specialities</span>
-                    <ul>
-                        <li class="speciality__tag-item">
-                            <span class="speciality__tag-language speciality__tag-language--html">HTML</span>
+                    <div class="github-user-profile__img-wrap">
+                        <img class="github-user-profile__img" src="https://avatars2.githubusercontent.com/u/10155855?s=460&v=4" alt=""/>
+                    </div>
+                        <div class="github-user-profile__info">
+                            <h1>Aurelian Spodarec</h1>
+                            <span>@AurelianSpodarec</span>
+                        </div>
+                    <div class="github-user-profile__bio">
+                        <p>Apparently, this user prefers to keep an air of mystery about them. - You can always visit my website and find out more about how I can help you! 🥇 💯 👍 🥇 💯</p>
+                    </div>
+                    <ul class="github-details">
+                        <li class="github-details__item">
+                            <span class="github-details__icon"><i class="fas fa-home"></i></span>
+                            <span class="github-details__name">LoveToCode</span>
                         </li>
-                        <li class="speciality__tag-item">
-                            <span class="speciality__tag-language speciality__tag-language--css">CSS</span>
+                        <li class="github-details__item">
+                            <span class="github-details__icon"><i class="fas fa-map-marker-alt"></i></span>
+                            <span class="github-details__name">Poland/UK</span>
                         </li>
-                        <li class="speciality__tag-item">
-                            <span class="speciality__tag-language speciality__tag-language--js">JavaScript</span>
+                        <li class="github-details__item">
+                            <span class="github-details__icon"><i class="far fa-envelope"></i></span>
+                            <a class="github-details__link" href="mailto:aurelianxspodarec@gmail.com">aurelianxspodarec@gmail.com</a>
                         </li>
-                        <li class="speciality__tag-item">
-                            <span class="speciality__tag-language speciality__tag-language--ruby">Ruby</span>
-                        </li>
-                        <li class="speciality__tag-item">
-                            <span class="speciality__tag-language speciality__tag-language--php">PHP</span>
+                        <li class="github-details__item">
+                            <span class="github-details__icon"><i class="fas fa-link"></i></span>
+                            <a class="github-details__link" href="http://aurelianspodarec.co.uk/">http://aurelianspodarec.co.uk/</a>
                         </li>
                     </ul>
-                </div>
+
+                    <div class="speciality">
+                        <span class="speciality__title">Topics & Specialities</span>
+                        <ul>
+                            <li class="speciality__tag-item">
+                                <span class="speciality__tag-language speciality__tag-language--html">HTML</span>
+                            </li>
+                            <li class="speciality__tag-item">
+                                <span class="speciality__tag-language speciality__tag-language--css">CSS</span>
+                            </li>
+                            <li class="speciality__tag-item">
+                                <span class="speciality__tag-language speciality__tag-language--js">JavaScript</span>
+                            </li>
+                            <li class="speciality__tag-item">
+                                <span class="speciality__tag-language speciality__tag-language--ruby">Ruby</span>
+                            </li>
+                            <li class="speciality__tag-item">
+                                <span class="speciality__tag-language speciality__tag-language--php">PHP</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div><!-- /github-user-profile__details -->
-              
+
+                    
+                <div class="github-user-profile__main-view">
+                    <h2>Pinned Repositories</h2>
+
+                    <ol class="pinned-repo-list">
+                        <li class="pinned-repo-item">
+                        <div class="pinned-repo-content">
+                            <span>
+                                <span>Dragg</span>
+                                <span>Webpack-to-WordPress-Starting-Template</span>
+                            </span>
+                            <p>
+                                <span>CSS</span>
+                                <span>Starred 4</span>
+                                <span>Forked 3</span>
+                            </p>
+                        </div>
+                        </li>
+                    </ol>
+                </div>
+
+
             </div><!-- /github-user-profile -->
 
-            <div>
-                <h2>Pinned Repositories</h2>
-
-                <ol class="pinned-repo-list">
-                    <li class="pinned-repo-item">
-                    <div class="pinned-repo-content">
-                        <span>
-                            <span>Dragg</span>
-                            <span>Webpack-to-WordPress-Starting-Template</span>
-                        </span>
-                        <p>
-                            <span>CSS</span>
-                            <span>Starred 4</span>
-                            <span>Forked 3</span>
-                        </p>
-                    </div>
-                    </li>
-                </ol>
-            </div>
 
         `
         return view;
