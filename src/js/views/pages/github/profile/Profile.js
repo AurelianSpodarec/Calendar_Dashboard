5e3f29331eb428cd6bbed5c74fd930cc53144849
Aurelian <aurelians@libero.it>
2019-01-24 15:41:14 +0000

    Created github nav HTML and CSS

--- src/js/views/pages/github/profile/Profile.js
+++ src/js/views/pages/github/profile/Profile.js
@@ -1,14 +1,59 @@
 let GithubProfile = {
     render : async () => {
         let view = /*html*/`
-            
+
+            <div class="github-nav">
+            <div class="github-nav__outer">
+
+                <div class="github-nav__profile-details">
+                    <img class="github-nav__profile-avatar" src="https://avatars2.githubusercontent.com/u/10155855?s=460&v=4" alt="" />
+                    <div class="github-nav__profile-info">
+                        <span class="github-nav__profile-name">Aurelian Spodarec</span>
+                        <span class="github-nav__profile-handler">@AurelianSpodarec</span>
+                    </div>
+                </div>
+
+                <ul class="github-nav__links">
+                    <li class="github-nav__item">
+                        <a class="github-nav__link" href="#tab1">
+                            <span class="github-nav__item-name">Overview</span>
+                        </a>
+                    </li>
+                    <li class="github-nav__item">
+                        <a class="github-nav__link" href="#tab1">
+                            <span class="github-nav__item-name">Repositories</span>
+                            <span class="github-nav__item-counter">77</span>
+                        </a>
+                    </li>
+                    <li class="github-nav__item">
+                        <a class="github-nav__link" href="#tab1">
+                            <span class="github-nav__item-name">Stars</span>
+                            <span class="github-nav__item-counter">44</span>
+                        </a>
+                    </li>
+                    <li class="github-nav__item">
+                        <a class="github-nav__link" href="#tab1">
+                            <span class="github-nav__item-name">Followers</span>
+                            <span class="github-nav__item-counter">12</span>
+                        </a>
+                    </li>
+                    <li class="github-nav__item">
+                        <a class="github-nav__link" href="#tab1">
+                            <span class="github-nav__item-name">Following</span>
+                            <span class="github-nav__item-counter">25</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+
             <h1> Github Profile </h1>
             
             <div class="github-user-profile">
 
-                <div>
-                    <div class="github-user-profile__img-wrap"><img src="https://avatars2.githubusercontent.com/u/10155855?s=460&v=4" alt=""/></div>
-                    <div class="github-user-profile__img">
+                <div class="github-user-profile__details">
+                    <div class="github-user-profile__img-wrap"><img class="github-user-profile__img" src="https://avatars2.githubusercontent.com/u/10155855?s=460&v=4" alt=""/></div>
+                    <div class="github-user-profile__info">
                         <h1>Aurelian Spodarec</h1>
                         <span>@AurelianSpodarec</span>
                     </div>
@@ -20,6 +65,7 @@ let GithubProfile = {
                     Apparently, this user prefers to keep an air of mystery about them. - You can always visit my website and find out more about how I can help you! 🥇 💯 👍 🥇 💯
                 </p>
                 </div>
+
                 <ul class="github-details">
                     <li class="github-details__item">
                         <span class="github-details__icon">Icon</span>
