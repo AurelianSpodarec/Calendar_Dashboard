3c87df8027553346f9f0a5ec8d461578fe70fa2e
Aurelian <aurelians@libero.it>
2019-02-16 03:49:33 +0000

    re-factored the calendar banner to switch and added transition

--- src/js/views/scenes/calendar/components/header/components/CalBannerImage.js
+++ src/js/views/scenes/calendar/components/header/components/CalBannerImage.js
@@ -6,28 +6,39 @@ class CalBannerImage extends Component {
         this.onEvent = this.onEvent.bind(this);
         this.setSubscriber("CalBannerImage", this.onEvent);
     }
- 
+
     renderBanner() {
-        if(this.getStoreState().calendar.currentMonthIndex === 11 ||
-            this.getStoreState().calendar.currentMonthIndex === 0 || 
-            this.getStoreState().calendar.currentMonthIndex === 1) {
+        const {currentMonthIndex} = this.getStoreState().calendar;
+        let imageUrl;
 
-            this.refs.bannerImg.style.backgroundImage = "url('https://c8.alamy.com/comp/PTB79P/vector-banner-blue-winter-background-with-ice-and-snow-PTB79P.jpg')";
-        } else if (this.getStoreState().calendar.currentMonthIndex === 2 ||
-            this.getStoreState().calendar.currentMonthIndex === 3 || 
-            this.getStoreState().calendar.currentMonthIndex === 4) {
-
-            this.refs.bannerImg.style.backgroundImage = "url('https://3.bp.blogspot.com/-YOTSOEh6API/XFeB3Zhj5KI/AAAAAAAAFhk/2u2qQ2QMWckkDz4yRnDeBRgaR7C98QPawCHMYCw/s1600/spring-painting-%25E2%259D%25A4-4k-hd-desktop-wallpaper-for-4k-ultra-hd-tv.jpg')";
-        } else if (this.getStoreState().calendar.currentMonthIndex === 5 ||
-            this.getStoreState().calendar.currentMonthIndex === 6 || 
-            this.getStoreState().calendar.currentMonthIndex === 7) {
-
-            this.refs.bannerImg.style.backgroundImage = "url('https://childrenstree.org/wp-content/uploads/2014/03/pink-flowers-summer-wallpapers-hd-wallpaper-wallpaper-nature-landscape-photo-summer-wallpaper-1024x640.jpg')";
-        } else if(this.getStoreState().calendar.currentMonthIndex === 8 ||
-            this.getStoreState().calendar.currentMonthIndex === 9 || 
-            this.getStoreState().calendar.currentMonthIndex === 10) {
-            this.refs.bannerImg.style.backgroundImage = "url('https://i.ytimg.com/vi/l5r6stay7ls/maxresdefault.jpg')";
+        switch(currentMonthIndex) {
+            case 11:
+            case 0:
+            case 1: {
+                imageUrl = "https://c8.alamy.com/comp/PTB79P/vector-banner-blue-winter-background-with-ice-and-snow-PTB79P.jpg";
+                break;
+            }
+            case 2:
+            case 3:
+            case 4: {
+                imageUrl = "https://3.bp.blogspot.com/-YOTSOEh6API/XFeB3Zhj5KI/AAAAAAAAFhk/2u2qQ2QMWckkDz4yRnDeBRgaR7C98QPawCHMYCw/s1600/spring-painting-%25E2%259D%25A4-4k-hd-desktop-wallpaper-for-4k-ultra-hd-tv.jpg";
+                break;
+            }
+            case 5:
+            case 6:
+            case 7: {
+                imageUrl = "https://childrenstree.org/wp-content/uploads/2014/03/pink-flowers-summer-wallpapers-hd-wallpaper-wallpaper-nature-landscape-photo-summer-wallpaper-1024x640.jpg";
+                break;
+            }
+            case 8:
+            case 9:
+            case 10: {
+                imageUrl = "https://i.ytimg.com/vi/l5r6stay7ls/maxresdefault.jpg";
+                break;
+            }
         }
+
+        this.refs.bannerImg.style.backgroundImage = `url('${imageUrl}')`;
     }
 
     onEvent(state, action) {
@@ -37,17 +48,6 @@ class CalBannerImage extends Component {
     onCreated() {
        this.renderBanner();
     }
-
-    /*
-
-        Winter 
-        Spring 
-        Summer 
-        Autumn 
-
-
-
-    */
 
     render() {
         return /*html*/`
