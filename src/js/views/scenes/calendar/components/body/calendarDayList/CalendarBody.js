9ca015d91424c804ddb4b5dbef31c64c8c58535c
Aurelian <aurelians@libero.it>
2019-02-21 04:25:32 +0000

    some changes

--- src/js/views/scenes/calendar/components/body/calendarDayList/CalendarBody.js
+++ src/js/views/scenes/calendar/components/body/calendarDayList/CalendarBody.js
@@ -4,7 +4,7 @@ import createElement from "#lib/createElement";
 class CalendarBody extends Component {
     constructor(props) {
         super(props);
-        this.fullYear = this.getStoreState().calendar.currentMonthIndex;
+        this.currentYear = this.getStoreState().calendar.currentYear;
         this.currentMonth = this.getStoreState().calendar.currentMonthIndex;
     }
 
@@ -19,9 +19,10 @@ class CalendarBody extends Component {
             let currentMonthDays = 1;
             let nextMonthDays = 1;
           
-            let daysInMonth = this.getDaysInMonth(this.currentMonth,  this.fullYear),
+            let daysInMonth = this.getDaysInMonth(this.currentMonth,  this.currentYear),
                 firstDayMonth = new Date( new Date().getFullYear(), this.currentMonth, 1),
                 firstDayWeekday = firstDayMonth.getDay(),
+                
                 prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
                 prev_year = prev_month == 11 ?  new Date().getFullYear() - 1 :  new Date().getFullYear(),
                 previousMonthDays = this.getDaysInMonth(prev_month, prev_year);
@@ -30,19 +31,17 @@ class CalendarBody extends Component {
                 
                 // Row
                 if (lastDayOfWeek == 0) {
-                    output += "<div data-timestamp=\"${new Date().toDateString()}\" class=\"cal__cell-row\">";
+                    output += "<div class=\"cal__cell-row\">";
                 }    
 
-                if(i < new Date( this.fullYear, this.currentMonth, 1).getDay()) {
-                    let ol =  (previousMonthDays - firstDayWeekday + i + 1)
-                    output += `${createElement(new CalendarDayItem({dayNumber: ol, otherMonth: true})).outerHTML} `
-
+                if(i < new Date( new Date().getFullYear(), new Date().getMonth(), 1).getDay()) {
+                    let day =  (previousMonthDays - firstDayWeekday + i + 1)
+                    output += `${createElement(new CalendarDayItem({ day, otherMonth: true })).outerHTML} `
                 } else if(currentMonthDays > daysInMonth) {
-                    output += `${createElement(new CalendarDayItem({dayNumber: nextMonthDays, otherMonth: true  })).outerHTML} `
+                    output += `${createElement(new CalendarDayItem({ day: nextMonthDays, otherMonth: true })).outerHTML} `
                     nextMonthDays++;
-
                 } else {
-                    output += `${createElement(new CalendarDayItem({dayNumber: currentMonthDays })).outerHTML} `
+                    output += `${createElement(new CalendarDayItem({ day: currentMonthDays })).outerHTML} `
                     currentMonthDays++;
                 }
 
--- src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
+++ src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
@@ -4,17 +4,21 @@ import createElement from "#lib/createElement";
 class CalendarDayItem extends Component {
     constructor(props) {
         super(props);
+        this.currentYear = this.getStoreState().calendar.currentYear;
+        this.currentMonth = this.getStoreState().calendar.currentMonthIndex;
+        this.setSubscriber("CalendarDayItem", this.onEvent);
     }
   
     render() {
-        let days = this.props.dayNumber;
-
+        let days = this.props.day;
         let otherMonth = this.props.otherMonth ? "is-other-month" : "";
-        // TODO: display the correct today date
+        // TODO: display the correct today date with a timestamp
         let today = (new Date().getDate() === days) ? "is-today" : "";
-
+        // new Date( new Date().getFullYear(), new Date().getMonth(), 1).getDay()
         return /*html*/`
-            <div class="cal__cell cal__cell-day ${today} ${otherMonth}" data-timestamp="${new Date().toDateString()}">
+            <div class="cal__cell cal__cell-day ${today} ${otherMonth}" 
+                date-timestamp="${new Date( this.currentYear, new Date().getMonth(), days).toDateString()}"
+            >
                 <div class="cal__cell-top">
                     <span class="cal__day-number">${days}</span>
                 </div>
