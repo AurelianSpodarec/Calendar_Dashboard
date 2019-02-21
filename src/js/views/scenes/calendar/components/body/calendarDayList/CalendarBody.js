122cd8800fcd897b83ae3f3bd227552f169c7699
Aurelian <aurelians@libero.it>
2019-02-21 17:29:29 +0000

    fixed the timestam for previous and next month, to the correct one

--- src/js/views/scenes/calendar/components/body/calendarDayList/CalendarBody.js
+++ src/js/views/scenes/calendar/components/body/calendarDayList/CalendarBody.js
@@ -12,37 +12,39 @@ class CalendarBody extends Component {
         return new Date(year, month + 1, 0).getDate();
     }
 
-    renderCalendarBody() {     
+    renderCalendarBody() {            
         let output = ""; 
 
-            let lastDayOfWeek = 0;
-            let currentMonthDays = 1;
-            let nextMonthDays = 1;
-          
-            let daysInMonth = this.getDaysInMonth(this.currentMonth,  this.currentYear),
-                firstDayMonth = new Date( new Date().getFullYear(), this.currentMonth, 1),
-                firstDayWeekday = firstDayMonth.getDay(),
-                
-                prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
-                prev_year = prev_month == 11 ?  new Date().getFullYear() - 1 :  new Date().getFullYear(),
-                previousMonthDays = this.getDaysInMonth(prev_month, prev_year);
-                  
+
+            var lastDayOfWeek = 0;
+            var currentMontDays = 1;
+            var nextMonthDays = 1;
+            
+            var daysInMonth = this.getDaysInMonth(this.getStoreState().calendar.currentMonthIndex,  new Date().getFullYear()),
+                firstDayMonth = new Date( new Date().getFullYear(), this.getStoreState().calendar.currentMonthIndex, 1),
+                firstDayWeekday = firstDayMonth.getDay();
+
+                var prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
+                    prev_year = prev_month == 11 ?  new Date().getFullYear() - 1 :  new Date().getFullYear(),
+                    previousMonthDays = this.getDaysInMonth(prev_month, prev_year);
+
             for (let i = 1; i < 36; i++) {
-                
+
                 // Row
                 if (lastDayOfWeek == 0) {
                     output += "<div class=\"cal__cell-row\">";
                 }    
 
-                if(i < new Date( new Date().getFullYear(), new Date().getMonth(), 1).getDay()) {
+                if(i < new Date( new Date().getFullYear(), this.getStoreState().calendar.currentMonthIndex, 1).getDay()) {
                     let day =  (previousMonthDays - firstDayWeekday + i + 1)
-                    output += `${createElement(new CalendarDayItem({ day, otherMonth: true })).outerHTML} `
-                } else if(currentMonthDays > daysInMonth) {
-                    output += `${createElement(new CalendarDayItem({ day: nextMonthDays, otherMonth: true })).outerHTML} `
+            
+                    output +=   `${createElement(new CalendarDayItem({ day, prevMonth: true, otherMonth: true})).outerHTML} `
+                } else if(currentMontDays > daysInMonth) {
+                    output +=   `${createElement(new CalendarDayItem({ day:  nextMonthDays, nextMonth: true,otherMonth: true  })).outerHTML} `
                     nextMonthDays++;
                 } else {
-                    output += `${createElement(new CalendarDayItem({ day: currentMonthDays })).outerHTML} `
-                    currentMonthDays++;
+                    output +=   `${createElement(new CalendarDayItem({ day: currentMontDays })).outerHTML} `
+                    currentMontDays++;
                 }
 
                 // Row
@@ -53,6 +55,7 @@ class CalendarBody extends Component {
                     lastDayOfWeek++;
                 }
             }
+
 
         return output;
     }
--- src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
+++ src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
@@ -15,10 +15,46 @@ class CalendarDayItem extends Component {
         // TODO: display the correct today date with a timestamp
         let today = (new Date().getDate() === days) ? "is-today" : "";
         // new Date( new Date().getFullYear(), new Date().getMonth(), 1).getDay()
+    //    let  prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
+
+    //    if(this.props.otherMonth) ? prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
+    //    :
+
+        // switch (action) {
+        //     case 'prev':
+        //     date = new Date(date.getFullYear(), date.getMonth() - 1, 1)
+        //     break
+        //        case 'current'
+        //     case 'next':
+        //     date = new Date(date.getFullYear(), date.getMonth() + 1, 1)
+        //     break
+        // }
+// is prevMonth - 1
+// is nextMonth + 1
+
+        let prevMonth = this.props.prevMonth ? this.currentMonth - 1 : this.currentMonth;
+        let nextMonth = this.props.nextMonth ? this.currentMonth + 1 : this.currentMonth;
+        let currentMonth = this.currentMonth;
+
+        let timeStampMonth = 3;
+
+
+        let curMonth;
+        if(this.props.prevMonth) {
+            curMonth = this.currentMonth - 1;
+        } else if (this.props.nextMonth) {
+            curMonth = this.currentMonth + 1;
+        } else {
+            curMonth = this.currentMonth;
+        }
+      
         return /*html*/`
-            <div class="cal__cell cal__cell-day ${today} ${otherMonth}" 
-                date-timestamp="${new Date( this.currentYear, new Date().getMonth(), days).toDateString()}"
+            <div 
+                class="cal__cell cal__cell-day ${today} ${otherMonth}" 
+                date-timestamp="${new Date( this.currentYear, curMonth, days).toDateString()}"
             >
+
+            
                 <div class="cal__cell-top">
                     <span class="cal__day-number">${days}</span>
                 </div>
