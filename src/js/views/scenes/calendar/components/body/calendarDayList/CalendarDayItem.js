af6b5c29e05d5f32a6ed114749e534f124b233fc
Aurelian <aurelians@libero.it>
2019-02-21 17:54:36 +0000

    cleaned zombie code

--- src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
+++ src/js/views/scenes/calendar/components/body/calendarDayList/CalendarDayItem.js
@@ -12,31 +12,9 @@ class CalendarDayItem extends Component {
     render() {
         let days = this.props.day;
         let otherMonth = this.props.otherMonth ? "is-other-month" : "";
+
         // TODO: display the correct today date with a timestamp
         let today = (new Date().getDate() === days) ? "is-today" : "";
-        // new Date( new Date().getFullYear(), new Date().getMonth(), 1).getDay()
-    //    let  prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
-
-    //    if(this.props.otherMonth) ? prev_month = this.getStoreState().calendar.currentMonthIndex == 0 ? 11 : this.getStoreState().calendar.currentMonthIndex - 1,
-    //    :
-
-        // switch (action) {
-        //     case 'prev':
-        //     date = new Date(date.getFullYear(), date.getMonth() - 1, 1)
-        //     break
-        //        case 'current'
-        //     case 'next':
-        //     date = new Date(date.getFullYear(), date.getMonth() + 1, 1)
-        //     break
-        // }
-// is prevMonth - 1
-// is nextMonth + 1
-
-        let prevMonth = this.props.prevMonth ? this.currentMonth - 1 : this.currentMonth;
-        let nextMonth = this.props.nextMonth ? this.currentMonth + 1 : this.currentMonth;
-        let currentMonth = this.currentMonth;
-
-        let timeStampMonth = 3;
 
 
         let curMonth;
@@ -53,8 +31,6 @@ class CalendarDayItem extends Component {
                 class="cal__cell cal__cell-day ${today} ${otherMonth}" 
                 date-timestamp="${new Date( this.currentYear, curMonth, days).toDateString()}"
             >
-
-            
                 <div class="cal__cell-top">
                     <span class="cal__day-number">${days}</span>
                 </div>
